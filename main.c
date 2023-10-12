67f297b37a6ba4ad1479deaf9663fc728cda41ab
Salma Sobhy <116264673+salma-4@users.noreply.github.com>
2023-10-12 22:38:12 +0300

    first_task

---
+++ main.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_USERS 100
+#define MAX_USERNAME_LENGTH 20
+#define MAX_PASSWORD_LENGTH 20
+
+struct User {
+    char username[MAX_USERNAME_LENGTH];
+    char password[MAX_PASSWORD_LENGTH];
+};
+
+struct User users[MAX_USERS];
+int numUsers = 0;
+
+int registerUser() {
+    if (numUsers >= MAX_USERS) {
+        printf("Maximum number of users reached.\n");
+        return 0;
+    }
+
+    printf("Enter username: ");
+    scanf("%s", users[numUsers].username);
+
+    printf("Enter password: ");
+    scanf("%s", users[numUsers].password);
+
+    numUsers++;
+    printf("Registration successful.\n");
+    return 1;
+}
+
+int loginUser() {
+    char username[MAX_USERNAME_LENGTH];
+    char password[MAX_PASSWORD_LENGTH];
+
+    printf("Enter username: ");
+    scanf("%s", username);
+
+    printf("Enter password: ");
+    scanf("%s", password);
+
+    for (int i = 0; i < numUsers; i++) {
+        if (strcmp(username, users[i].username) == 0 && strcmp(password, users[i].password) == 0) {
+            printf("Login successful.\n");
+            return 1;
+        }
+    }
+
+    printf("Invalid username or password.\n");
+    return 0;
+}
+
+int main() {
+    int choice;
+
+    do {
+        printf("Enter 1 to Register:\n");
+        printf("Enter 2 to Login:\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                registerUser();
+                break;
+            case 2:
+                loginUser();
+                break;
+            default:
+                printf("Invalid choice. Please try again.\n");
+        }
+
+        printf("\n");
+    } while (choice != 3);
+
+    return 0;
+}
